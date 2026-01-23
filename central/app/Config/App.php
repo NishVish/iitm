418f1d4200074579f64daa42ed2341243c10a735
Nishant Vishwakarma <42230092+NishVish@users.noreply.github.com>
2026-01-23 16:20:34 +0530

    no change to this file

--- central/app/Config/App.php
+++ central/app/Config/App.php
@@ -17,6 +17,7 @@ class App extends BaseConfig
      * E.g., http://example.com/
      */
 public string $baseURL = 'http://localhost/iitm/central/';
+
 //public string $baseURL = 'https://iitmindia.com/ci/central/';
 
     /**
