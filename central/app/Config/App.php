0f5ff4be066956222b4f20a32d83542b4a3d072e
Nishant Vishwakarma <42230092+NishVish@users.noreply.github.com>
2026-01-23 15:08:23 +0530

    url changes

--- central/app/Config/App.php
+++ central/app/Config/App.php
@@ -16,8 +16,8 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-public string $baseURL = 'http://localhost/central/';
-//public string $baseURL = 'https://iitmindia.com/ci/central/';
+//public string $baseURL = 'http://localhost/central/';
+public string $baseURL = 'https://iitmindia.com/ci/central/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
