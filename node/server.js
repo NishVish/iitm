0c1bd0285be70a3e0aec8e862507c2047cbd9172
Nishant Vishwakarma <42230092+NishVish@users.noreply.github.com>
2026-03-24 17:21:11 +0530

    udpade serv to local

--- node/server.js
+++ node/server.js
@@ -10,12 +10,12 @@ app.use(cors());
 app.use(express.json());
 
 const dbConfig = {
-    // host: "localhost",
+    host: "localhost",
     // user: "root",
     // password: "",
     // database: "testing_server"
 
-    host: "21.157.66.148.host.secureserver.net",
+    // host: "21.157.66.148.host.secureserver.net",
     port: 3306,
     user: "iitminda_master",
     password: "gB)%gU}ocn?MCP=} ", // Double check if there's a space at the end!
