b136ec8c95db75705e57b958655e6e07a80154f6
Nishant Vishwakarma <42230092+NishVish@users.noreply.github.com>
2026-04-25 12:13:43 +0530

    final test

--- lara/app/Http/Controllers/MailerController.php
+++ lara/app/Http/Controllers/MailerController.php
@@ -20,7 +20,7 @@ class MailerController extends Controller
         $headers .= "MIME-Version: 1.0\r\n";
         $headers .= "Content-type: text/html; charset=UTF-8\r\n";
 
-        $sent = mail('marketing1@iitminda.com', "test", "test", $headers);
+        $sent = mail('marketing1@iitmindia.com', "test", "test", $headers);
         if ($sent) {
             return back()->with('status', 'Mail Sent');
         } else {
