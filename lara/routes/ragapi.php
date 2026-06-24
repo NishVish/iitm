5875e4cc902f7765bfd7da60dde7bad8941f0bf2
Nishant Vishwakarma <42230092+NishVish@users.noreply.github.com>
2026-06-24 15:50:38 +0530

    chante

--- lara/routes/ragapi.php
+++ lara/routes/ragapi.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Ai\RagApi;
 
-Route::get('/ai/rag/update-data', [RagApi::class, 'updateData']);
+Route::get('/ai/rag/updatedata', [RagApi::class, 'updateData']);
 Route::match(['get', 'post'], '/ai/rag/ask', [RagApi::class, 'ask']);
 Route::get('/ai/rag/test', [RagApi::class, 'ragTest']);
 Route::get('/ragapi', [RagApi::class, 'ragapi']);
