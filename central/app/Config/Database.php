537bcce08bf0b7eed1d7c1bf0788c3c57f7aa2f4
Nishant Vishwakarma <42230092+NishVish@users.noreply.github.com>
2026-08-17 12:05:05 +0530

    connection

--- central/app/Config/Database.php
+++ central/app/Config/Database.php
@@ -29,22 +29,22 @@ class Database extends Config
 
 
 
-        // 'hostname' => '21.157.66.148.host.secureserver.net',
-        // 'username' => 'iitminda_master',
-        // 'password' => 'gB)%gU}ocn?MCP=}',
-        // 'database' => 'iitminda_testing_server',
-
-        //'hostname' => 'localhost', 'username' => 'root', 'password' => '', 'database' => 'iitminda_testing_server',
-
-
-
-        'hostname' => 'localhost',
-        'username' => 'root',
-        'password' => '',
-
-        // 'database' => 'localtesting',
-
+        'hostname' => '21.157.66.148.host.secureserver.net',
+        'username' => 'iitminda_master',
+        'password' => 'gB)%gU}ocn?MCP=}',
         'database' => 'iitminda_testing_server',
+
+        // 'hostname' => 'localhost', 'username' => 'root', 'password' => '', 'database' => 'iitminda_testing_server',
+
+
+
+        // 'hostname' => 'localhost',
+        // 'username' => 'root',
+        // 'password' => '',
+
+        // // 'database' => 'localtesting',
+
+        // 'database' => 'iitminda_testing_server',
 
 
 
